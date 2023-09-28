4d6e732359caf1b11bed3129331b8f7fabf54a99
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-09-28 04:04:49 +0000

    Added Base Page Template

---
+++ valencyapp/base_page.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:valencyapp/main.dart';
+
+class BasePage extends StatefulWidget {
+  const BasePage({super.key});
+  @override
+  _BasePageState createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
+  /* Variables Go Here */
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+  
+  }
+}
