4fcda01294cef65f764a0dabe2ced130064eec41
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-11-26 22:31:20 +1000

    Added Structure for 3 more screens

---
+++ valencyapp/signup_confirmation_email_screen.dart
@@ -0,0 +1,8 @@
+// Button - Back Icon (to go back to signup_tos_screen)
+
+// Title Text - Verify your email
+// Small Text - Please enter the 4 digit code sent to ENTER-EMAIL-HERE
+
+// Input Field - Verification Code
+
+// Button (looks like text) - Resend Code (to send another email)
