fd42007f30fde32c1c70e2f34f30413cbb2e4f93
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-11-26 22:40:32 +1000

    Added structures for sign in

---
+++ valencyapp/signin_screen.dart
@@ -0,0 +1,9 @@
+// Title Text - Login
+// Small Text - Please sign in to continue.
+
+// Input Field - Email
+// Input Field - Password
+// Button (looks like text) - Forgot password?
+
+// Button - LOGIN
+// Button (looks like text) - Don't have an account? Sign up
