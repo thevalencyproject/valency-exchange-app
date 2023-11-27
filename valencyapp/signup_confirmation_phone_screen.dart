70fbca7a05bfc691add8439b88d2f682cd6ead68
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-11-27 17:03:06 +1000

    Delete valencyapp/signup_confirmation_phone_screen.dart

--- valencyapp/signup_confirmation_phone_screen.dart
+++
@@ -1,8 +0,0 @@
-// Button - Back Icon (to go back to signup_tos_screen)
-
-// Title Text - Verify your phone number
-// Small Text - Please enter the 4 digit code sent to ENTER-PHONE-NUMBER-HERE
-
-// Input Field - Verification Code
-
-// Button (looks like text) - Resend Code (to send another sms)
