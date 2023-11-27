8f1d7625d1cc5d55c28a0f033595bc5d93abf78a
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-11-27 17:01:09 +1000

    Added Signup Section

---
+++ valencyapp/signup/signup_tos_screen.dart
@@ -0,0 +1,15 @@
+// Title Text - Terms of Service
+// Small Text - You must read and agree to the terms of service to use our exchange
+// Button - Back Icon (to go back to signup_screen)
+
+// Input Field - Full Name
+// Input Field - Phone Number
+// Input Field - Email
+// Input Field - Address
+// Input Field - Password
+// Input Field - Confirm Pasword
+
+// Display Field - Shows the terms of service (connected to database to update whenever TOS updates)
+
+// Button - Disagree
+// Button - Agree
