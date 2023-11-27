8609575adbb8bb69e7f0a2396478918e19292d5f
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-11-27 17:03:28 +1000

    Delete valencyapp/signup_kyc_face_screen.dart

--- valencyapp/signup_kyc_face_screen.dart
+++
@@ -1,8 +0,0 @@
-// Title Text - Know Your Customer
-// Small Text Bold - Take a selfie with you and your ID.
-// Small Text - Ensure the photo is clear and ID is visible.
-// Button - Back Icon (to go back to signup_tos_screen)
-
-// Camera Field - Overlays for Face Outline and ID Outline
-
-// Button - CONTINUE
