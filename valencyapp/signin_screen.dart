86d36a633d4a7964022fcc84fbcbe53a3c3bcfa5
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-11-27 17:02:51 +1000

    Delete valencyapp/signin_screen.dart

--- valencyapp/signin_screen.dart
+++
@@ -1,9 +0,0 @@
-// Title Text - Login
-// Small Text - Please sign in to continue.
-
-// Input Field - Email
-// Input Field - Password
-// Button (looks like text) - Forgot password?
-
-// Button - LOGIN
-// Button (looks like text) - Don't have an account? Sign up
