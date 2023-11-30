1ca265a41d63221952f7321dfab8c1a524033fce
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-11-30 20:52:04 +1000

    Displays SVG Back Button

---
+++ valencyapp/components/backbutton.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart'
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ValencyBackButton extends StatelessWidget {
+  final Function()? onTap;  // Detects button presses
+
+  const ValencyBackButton({
+    super.key,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: SvgPicture.asset(
+        'components/images/back-arrow.svg',
+        semanticsLabel: 'Back Button',
+        height: 100,
+        width: 100,
+      ),
+    );
+  }
+}
