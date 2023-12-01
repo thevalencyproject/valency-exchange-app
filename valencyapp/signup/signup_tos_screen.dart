44f6a6262bbba26ee086f2f9bd4bb8dd624c0f40
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-01 18:06:57 +1000

    Implemented a TOS screen for signup (minus TOS widget)

--- valencyapp/signup/signup_tos_screen.dart
+++ valencyapp/signup/signup_tos_screen.dart
@@ -13,3 +13,74 @@
 
 // Button - Disagree
 // Button - Agree
+
+import 'package:flutter/material.dart'
+import 'package:valencyapp/components/bigbutton.dart'
+import 'package:valencyapp/components/backbutton.dart'
+
+class SignUpTOSScreen extends StatelessWidget {
+  SignUpTOSScreen({super.key});
+
+  // I/O Controllers
+  void agree() {}      // Called when the agree button is pressed
+  void disagree() {}   // Called when the disagree button is pressed
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: const[
+
+
+              
+              const FractionallySizedBox(widthFactor: 1, heightFactor: 0.08),  // Gap between top and content
+
+              ValencyBackButton(
+                onTap: goBack,
+              ),
+
+              Text('Terms of Service', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 96
+                ),
+              ),
+              Text('You must read and agree to the terms of service to use our exchange', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 48
+                ),
+              ),
+
+              const SizedBox(height: 10),   // Gap text and terms of service
+
+              ValencyTOS(),
+
+              const SizedBox(height: 25),   // Gap between terms of service and buttons
+
+              ValencyBigButton(
+                onTap: disagree, 
+                buttonColor: Colors.grey, 
+                hintText: 'DISAGREE'
+              ),
+
+              const SizedBox(height: 10),   // Gap between buttons
+
+              ValencyBigButton(
+                onTap: agree, 
+                buttonColor: Colors.blue, 
+                hintText: 'AGREE'
+              ),
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
