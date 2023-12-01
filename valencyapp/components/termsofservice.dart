bcf8096b9b3634111c8de6159c58c3deac96c687
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-01 18:22:08 +1000

    Added a base for the terms of service widget

---
+++ valencyapp/components/termsofservice.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart'
+
+class ValencyTOS extends StatelessWidget {
+
+  const ValencyTOS({ super.key, });
+
+  @override
+  Widget build(BuildContext context) {
+    Container(
+      padding: const EdgeInsets.all(25),
+      margin: const EdgeInsets.symmetric(horizontal: 25),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+}
+
+// STILL NEEDS TO GET AND DISPLAY ACTUAL TOS + MUST BE CHANGED TO CORRECT SIZE (TAKE UP MOST OF SCREEN LIKE DESIGN)
