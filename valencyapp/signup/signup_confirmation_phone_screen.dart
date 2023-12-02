055dbd0f4389a328341994755425e025c6c580e7
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-02 14:11:07 +1000

    Added phone screen confirmation

--- valencyapp/signup/signup_confirmation_phone_screen.dart
+++ valencyapp/signup/signup_confirmation_phone_screen.dart
@@ -1,8 +1,70 @@
-// Button - Back Icon (to go back to signup_tos_screen)
+import 'package:flutter/material.dart'
+import 'package:valencyapp/components/textfield.dart'
+import 'package:valencyapp/components/textbutton.dart'
+import 'package:valencyapp/components/backbutton.dart'
 
-// Title Text - Verify your phone number
-// Small Text - Please enter the 4 digit code sent to ENTER-PHONE-NUMBER-HERE
+class SignUpConfirmationEmailScreen extends StatelessWidget {
+  SignUpConfirmationEmailScreen({super.key});
 
-// Input Field - Verification Code
+  // I/O Controllers
+  final verificationCodeController = TextEditingController();   // Reads verification code
+  void resendcode() {}                                          // Called when resendcode is pressed
+  void goBack() {}                                              // Called when back button is pressed
 
-// Button (looks like text) - Resend Code (to send another sms)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: const[
+
+
+              
+              const FractionallySizedBox(widthFactor: 1, heightFactor: 0.08),   // Gap top and content
+
+              ValencyBackButton(
+                onTap: goBack,
+              ),
+
+              const FractionallySizedBox(widthFactor: 1, heightFactor: 0.37),   // Gap back button and text
+
+              Text('Verify your phone number', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 96
+                ),
+              ),
+              Text("Please enter the 4 digit code sent to +61 0411 111 111", 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 48
+                ),
+              ),
+
+              const SizedBox(height: 20),   // Gap between Text and Text Fields
+
+              ValencyTextField(    // Input Field - Verification Code
+                controller: verificationCodeController,
+                hintText: 'Verification Code',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),   // Gap between text fields and text
+
+              ValencyTextButton(
+                onTap: resendcode,
+                buttonColor: Colors.blue,
+                buttonText: "Resend Code",
+              ),
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
