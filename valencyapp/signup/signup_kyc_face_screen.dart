ba76668cbd915d786127dea86e4b31f5f933427e
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-03 12:40:40 +1000

    Implemented KYC Sign Up Selfie Screen

--- valencyapp/signup/signup_kyc_face_screen.dart
+++ valencyapp/signup/signup_kyc_face_screen.dart
@@ -1,8 +1,62 @@
-// Title Text - Know Your Customer
-// Small Text Bold - Take a selfie with you and your ID.
-// Small Text - Ensure the photo is clear and ID is visible.
-// Button - Back Icon (to go back to signup_tos_screen)
+import 'package:flutter/material.dart'
+import 'package:valencyapp/components/backbutton.dart'
+import 'package:valencyapp/components/kyccamera.dart'
 
-// Camera Field - Overlays for Face Outline and ID Outline
+class SignUpKYCFaceScreen extends StatelessWidget {
+  SignUpKYCFaceScreen({super.key});
 
-// Button - CONTINUE
+  // I/O Controllers
+  void goBack() {}   // Called when back button is pressed
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: const[
+
+
+              
+              const FractionallySizedBox(widthFactor: 1, heightFactor: 0.08),  // Gap between top and content
+
+              ValencyBackButton(
+                onTap: goBack,
+              ),
+
+              Text('Know Your Customer', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 96
+                ),
+              ),
+              Text('Take a selfie with you and your ID.', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 48,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text('Ensure the photo is clear and ID is visible.', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 48
+                ),
+              ),
+
+              const SizedBox(height: 10),   // Gap text and terms of service
+
+              ValencyKYCCamera(   // Displays the KYC camera - set to take front license photo
+                photoType: 2,     // 0 = Front Licence, 1 = Back License, 2 = Face Photo
+              ),
+
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
