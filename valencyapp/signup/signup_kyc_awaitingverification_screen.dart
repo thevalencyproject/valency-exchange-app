5ea2fd00b26274883232fa80ecde22461a40da23
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-03 13:23:19 +1000

    Implemented Awaiting Verification Screen

--- valencyapp/signup/signup_kyc_awaitingverification_screen.dart
+++ valencyapp/signup/signup_kyc_awaitingverification_screen.dart
@@ -3,3 +3,64 @@
 // Small Text - This can take anywhere from 1 minute to 2 hours. You can close the app in the meantime.
 
 // Loading Icon - Loads until account has been verified
+import 'package:flutter/material.dart'
+
+class SignUpAwaitingVerificationScreen extends StatelessWidget {
+  SignUpAwaitingVerificationScreen({super.key});
+
+  // I/O Controllers
+  void goBack() {}   // Called when back button is pressed
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: const[
+
+
+              
+              const FractionallySizedBox(widthFactor: 1, heightFactor: 0.33),  // Gap between top and content
+
+              Text('Awaiting Verification', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 96
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text('We are waiting for our KYC providers systems to validate your identity.', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 48,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Text('This can take anywhere from 1 minute to 2 hours. You can close the app in the meantime.', 
+                style: TextStyle(
+                  color: Colors.black, 
+                  fontSize: 48
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(height: 25),   // Gap text and terms of service
+
+              // Loading GIF goes here
+              Image.asset(alignment: ,
+                "images/loading.gif",
+                height: 250,
+                width: 250,
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
