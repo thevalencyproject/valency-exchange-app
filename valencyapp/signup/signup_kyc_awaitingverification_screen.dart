40a2df80dc4dfc3476a0e5c96b0ce5f2efc5fd24
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-04 13:56:01 +1000

    Removed Template

--- valencyapp/signup/signup_kyc_awaitingverification_screen.dart
+++ valencyapp/signup/signup_kyc_awaitingverification_screen.dart
@@ -1,8 +1,3 @@
-// Title Text - Awaiting Verification
-// Small Text Bold - We are waiting for our KYC providers systems to validate your identity.
-// Small Text - This can take anywhere from 1 minute to 2 hours. You can close the app in the meantime.
-
-// Loading Icon - Loads until account has been verified
 import 'package:flutter/material.dart'
 
 class SignUpAwaitingVerificationScreen extends StatelessWidget {
