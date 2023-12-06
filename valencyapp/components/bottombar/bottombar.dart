7da3c869c2186757f2a37efd58e997e51707e09b
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-06 17:20:48 +1000

    Implemented BottomBar (still required formatting)

--- valencyapp/components/bottombar/bottombar.dart
+++ valencyapp/components/bottombar/bottombar.dart
@@ -8,23 +8,44 @@ class ValencyBigButton extends StatelessWidget {
     required this.focusedIcon,
   });
 
+  void MyPortfolioPressed() {
+
+  }
+  void ExplorePressed() {
+    
+  }
+  void MyWalletPressed() {
+    
+  }
+  void MyPositionsPressed() {
+    
+  }
+  void SettingsPressed() {
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     // Each button should be next to each other on the horizontal plane like in the designs
     ValencyMyPortfolioButton(
-      
+      onTap: MyPortfolioPressed,
+      active: if(focusedIcon == 0) { true } else { false },
     ),
     ValencyExploreButton(
-
+      onTap: ExplorePressed,
+      active: if(focusedIcon == 1) { true } else { false },
     ),
     ValencyMyWalletButton(
-
+      onTap: MyWalletPressed,
+      active: if(focusedIcon == 2) { true } else { false },
     ),
     ValencyMyPositionsButton(
-
+      onTap: MyPositionsPressed,
+      active: if(focusedIcon == 3) { true } else { false },
     ),
     ValencySettingsButton(
-
+      onTap: SettingsPressed,
+      active: if(focusedIcon == 4) { true } else { false },
     ),
   }
 }
