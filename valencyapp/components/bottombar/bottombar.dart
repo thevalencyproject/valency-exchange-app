ba3c6b5040093624af39c48ab275c91be18ff583
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-06 21:11:17 +1000

    Fixed Conditionals for bottom bar base widget

--- valencyapp/components/bottombar/bottombar.dart
+++ valencyapp/components/bottombar/bottombar.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart'
+import 'package:valencyapp/components/bottombar/myportfoliobutton.dart'
+import 'package:valencyapp/components/bottombar/explorebutton.dart'
+import 'package:valencyapp/components/bottombar/mywalletbutton.dart'
+import 'package:valencyapp/components/bottombar/mypositionsbutton.dart'
+import 'package:valencyapp/components/bottombar/settingsbutton.dart'
 
 class ValencyBigButton extends StatelessWidget {
   final int focusedIcon;    // The button that is in focus: 0 = My Portfolio, 1 = Explore, 2 = My Wallet, 3 = My Positions, 4 = Settings
@@ -8,44 +13,46 @@ class ValencyBigButton extends StatelessWidget {
     required this.focusedIcon,
   });
 
-  void MyPortfolioPressed() {
-
-  }
-  void ExplorePressed() {
-    
-  }
-  void MyWalletPressed() {
-    
-  }
-  void MyPositionsPressed() {
-    
-  }
-  void SettingsPressed() {
-    
-  }
+  // I/O Controllers
+  void MyPortfolioPressed() {}  // Switch to my portfolio (if not already there)
+  void ExplorePressed() {}      // Switch to explore (if not already there)
+  void MyWalletPressed() {}     // Switch to my wallet (if not already there)
+  void MyPositionsPressed() {}  // Switch to my positions (if not already there)
+  void SettingsPressed() {}     // Switch to settings (if not already there)
 
   @override
   Widget build(BuildContext context) {
     // Each button should be next to each other on the horizontal plane like in the designs
+    
     ValencyMyPortfolioButton(
       onTap: MyPortfolioPressed,
-      active: if(focusedIcon == 0) { true } else { false },
+      active: focusedIcon == 0?
+        true:
+        false,
     ),
     ValencyExploreButton(
       onTap: ExplorePressed,
-      active: if(focusedIcon == 1) { true } else { false },
+      active: focusedIcon == 1?
+        true:
+        false,
     ),
     ValencyMyWalletButton(
       onTap: MyWalletPressed,
-      active: if(focusedIcon == 2) { true } else { false },
+      active: focusedIcon == 2?
+        true:
+        false,
     ),
     ValencyMyPositionsButton(
       onTap: MyPositionsPressed,
-      active: if(focusedIcon == 3) { true } else { false },
+      active: focusedIcon == 3?
+        true:
+        false,
     ),
     ValencySettingsButton(
       onTap: SettingsPressed,
-      active: if(focusedIcon == 4) { true } else { false },
+      active: focusedIcon == 4?
+        true:
+        false,
     ),
   }
 }
