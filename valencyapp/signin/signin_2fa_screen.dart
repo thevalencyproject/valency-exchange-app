1e0ccc05a501312b8b3ac28ea3b424ed67e5b760
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-07 22:11:55 +1000

    Fixed Import Issue

--- valencyapp/signin/signin_2fa_screen.dart
+++ valencyapp/signin/signin_2fa_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart'
-import 'package:valencyapp/components/textfield.dart'
-import 'package:valencyapp/components/textbutton.dart'
-import 'package:valencyapp/components/backbutton.dart'
+import 'package:flutter/material.dart';
+import 'package:valencyapp/components/textfield.dart';
+import 'package:valencyapp/components/textbutton.dart';
+import 'package:valencyapp/components/backbutton.dart';
 
 class SignIn2FAScreen extends StatelessWidget {
   SignIn2FAScreen({super.key});
