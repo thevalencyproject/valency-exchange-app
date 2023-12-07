f50b4eafe67ba437f6d286dd89787a490df633cc
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-07 22:03:07 +1000

    Fixed import issue

--- valencyapp/components/banking/fiatamountselector.dart
+++ valencyapp/components/banking/fiatamountselector.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class ValencyFIATAmountSelector extends StatelessWidget {
@@ -8,7 +8,7 @@ class ValencyFIATAmountSelector extends StatelessWidget {
   const ValencyFIATAmountSelector({
     super.key,
     required this.controller,
-    required this.widthrawal,
+    required this.withdrawal,
   });
 
   @override
