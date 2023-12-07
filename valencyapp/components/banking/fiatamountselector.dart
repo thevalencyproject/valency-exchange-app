67d154be25aacf172a4fa206d19b89bfd2185f71
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-07 21:04:04 +1000

    Added template for the fiat amount selector widget

---
+++ valencyapp/components/banking/fiatamountselector.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart'
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ValencyFIATAmountSelector extends StatelessWidget {
+  final controller;       // The controller that can access the contents of the input field
+  final bool withdrawal;  // True if withdrawal (for calculating what balance will be after transaction)
+
+  const ValencyFIATAmountSelector({
+    super.key,
+    required this.controller,
+    required this.widthrawal,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
