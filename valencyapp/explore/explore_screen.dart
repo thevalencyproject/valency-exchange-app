2ab9d6c31341b68f4572cb46d7b90d4179dcc3b9
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-14 23:07:49 +1000

    Added template for backend variables

---
+++ valencyapp/explore/explore_screen.dart
@@ -0,0 +1,5 @@
+// Variables given to the explore screen when drawing to screen:
+// 1. FIAT Balance Amount (for top right display)
+// 2. This months 6 popular lists - lists and icons are embedded in the application, so no need to use bandwidth for them
+// 3. Assets that are trending today (a list of multiple) - icons are embedded in the application, so no need to use bandwidth for them
+// 4. Current cryptocurrency news segment (all 3) - Title, link, description, and image for each
