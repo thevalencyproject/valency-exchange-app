83338e01a5232b31ab28ff6ba05c4f463982680b
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-15 17:11:30 +1000

    Added ValueGraph Directory

---
+++ valencyapp/components/main/valuegraph/valuegraph.dart
@@ -0,0 +1,5 @@
+// 3 Options: range buttons on bottom of graph, range buttons on top of graph, or no range buttons showing
+//           AND
+//            opening rate for positions showing or not showing
+//           AND
+//            percentage change over past range on top of graph, or no percentage change
