2d341f92e3937b5f7421184dbbc2cd2072017474
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-17 17:07:47 +1000

    Added child components for the valuegraph (just structures)

---
+++ valencyapp/components/main/valuegraph/basegraph.dart
@@ -0,0 +1,3 @@
+// OPTIONS:
+// 1. opening rate for positions showing or not showing
+// 2. if graph dynamically updates or not (if so, it uses a ValueListenableBuilder)
---
+++ valencyapp/components/main/valuegraph/rangebuttons.dart
@@ -0,0 +1,1 @@
+// Uses controllers to detect button presses
