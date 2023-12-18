e460c68041acc3b5ea6e2106e11a17aea0b15990
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-18 14:08:02 +1000

    Outlined functionality of the searchbar + input variables

--- valencyapp/components/main/search.dart
+++ valencyapp/components/main/search.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 
 // This widget is responsible for search functionality
-// Takes in: All searchable terms and where to go on each search
+// Name: ValencySearchBar
+// Takes in: All searchable terms
+//           All searchable terms price
+// WHERE TO GO ON EACH SEARCH IS DONE BY INPUTTING THE SELECTED SEARCHABLE TERM (AKA. BTC) INTO THE BUYASSET SCREEN INPUT (STILL NEEDS TO BE IMPLEMENTED)
+
+// Input Variables: searchable, searchablePrices, searchablePercentages
+// searchable (string to identify assets), searchablePrices (current prices of these assets), searchablePercentages (a percentage change of each asset over a specific time period)
