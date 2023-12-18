30ada0d8d9ade4b398cbd196cc6314b0cab775f9
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-18 21:37:21 +1000

    Implemented ValencyBaseGraph for the ValencyValueGraph Widget
    This uses the fl_chart flutter library for graph drawing functionality

--- valencyapp/components/main/valuegraph/basegraph.dart
+++ valencyapp/components/main/valuegraph/basegraph.dart
@@ -1,3 +1,58 @@
-// OPTIONS:
-// 1. opening rate for positions showing or not showing
-// 2. if graph dynamically updates or not (if so, it uses a ValueListenableBuilder)
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+enum SelectedRange { day, week, month, threeMonths, year, max }
+
+// Opening rate is optional - range should be a selection from SelectedRange, and data is a list of doubles over said time period
+class ValencyBaseGraph extends StatelessWidget {
+  const ValencyBaseGraph({
+    Key? key,
+    required this.data,
+    required this.selectedRange,
+    this.openingRate,
+  }) : super(key: key);
+
+  final List<double> data;
+  final SelectedRange selectedRange;
+  final double? openingRate;
+
+  @override
+  Widget build(BuildContext context) {
+    return LineChart(
+      LineChartData(
+        gridData: FlGridData(show:false),
+        titlesData: FlTitlesData(show: false),
+        borderData: FlBorderData(show: false),
+        lineBarsData: [
+          LineChartBarData(
+            spots: _getSpots(data),
+            isCurved: true,
+            barWidth: 2,
+            colors: [Colors.green],
+          ),
+        ],
+
+        extraLinesData: _getExtraLines(),
+      ),
+    );
+  }
+
+  List<FlSpot> _getSpots(List<double> data) {
+    return data.asMap().entries.map((e) => FlSpot(e.key.toDouble(), e.value)).toList();
+  }
+
+  ExtraLinesData _getExtraLines() {
+    return openingRate != null
+      ? ExtraLinesData(
+        horizontalLines: [
+          HorizontalLine(
+            y: openingRate!,
+            color: Colors.grey
+            strokeWidth: 1,
+            dashArray: [5, 5],
+          ),
+        ],
+      )
+    : ExtraLinesData();
+  }
+}
