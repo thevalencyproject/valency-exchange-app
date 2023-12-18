af9fdbb9092bcb76b9ed20fc1e579948fad1d0b8
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-18 15:19:06 +1000

    Implemented basic search suggestions logic

--- valencyapp/components/main/search.dart
+++ valencyapp/components/main/search.dart
@@ -8,3 +8,31 @@ import 'package:flutter/material.dart';
 
 // Input Variables: searchable, searchablePrices, searchablePercentages
 // searchable (string to identify assets), searchablePrices (current prices of these assets), searchablePercentages (a percentage change of each asset over a specific time period)
+class ValencySearchBar extends StatefulWidget {
+  ValencySearchBar({
+    super.key,
+    required this.searchable,
+    this.searchablePrices,
+    this.searchablePercentages,
+  });
+
+  final List<String> searchable = [];
+  final List<double> searchablePrices = [];
+  final List<double> searchablePercentages = [];
+  final searchBarInput = TextEditingController();   // Reads input in the text field
+
+  // Below is refreshed each time a new character is typed into the text field
+  List<String> suggestions = [];          // Generate a new suggestions list to go with the input
+  for(int i = 0; i < searchable.length; i++) {
+    String temp = searchable(i);                   // Create a temporary string to hold the searchable term
+    temp.substring(1, /*SEARCHBARINPUT.LENGTH*/)   // Size temp to the input length
+    if(/* SEARCHBARINPUT */ == temp) {             // If the input is equal to part of the searchable, add the full searchable term to suggestions
+      suggestions.add(searchable(i));
+    }
+  }
+  if(suggestions.length == 0) {   // If there are no suggestions based on the input
+    // DISPLAY NO SEARCH RESULTS TEXT HERE
+  } else {
+    // DISPLAY SUGGESTIONS LIST HERE
+  }
+}
