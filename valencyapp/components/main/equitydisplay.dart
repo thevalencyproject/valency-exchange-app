3017bfc7a169510fdf42d24b7abf2901b8a9c7af
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-19 14:24:02 +1000

    Properly implemented an equity display

--- valencyapp/components/main/equitydisplay.dart
+++ valencyapp/components/main/equitydisplay.dart
@@ -3,24 +3,32 @@
 //            Equity/Available Switching or No Equity/Available Switching
 import 'package:flutter/material.dart';
 
-class ValencyEquityDisplay extends StatelessWidget {
-  final String topText;               // The text above the equity/available display (usually Title Text for the entire screen)
-  final double equity;                // The equity amount 
-  final double available;             // The available amount
-  final bool switching;               // True if switching is available
-  final bool isAvailableDisplaying;   // True when the available amount is displaying
-
+class ValencyEquityDisplay extends StatefulWidget {
   ValencyEquityDisplay({
     super.key,
     required this.topText,
     required this.equity,
     required this.available,
     required this.switching,
-    required this.isAvailableDisplaying,
   });
 
-  // I/O Controllers
-  void switchType() {}          // Called when switch avaliable/equity button is pressed - redraws display with other type
+  final String topText;               // The text above the equity/available display (usually Title Text for the entire screen)
+  final double equity;                // The equity amount 
+  final double available;             // The available amount
+  final bool switching;               // True if switching is available
+
+  @override
+  State<ValencyEquityDisplay> createState() => _ValencyEquityDisplayState();
+}
+
+class _ValencyEquityDisplayState extends State<ValencyEquityDisplay> {
+  bool isAvailableDisplaying = false;   // Equity gets displayed by default
+
+  void switchType() {
+    setState(() {
+      isAvailableDisplaying = !isAvailableDisplaying;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,62 +37,27 @@ class ValencyEquityDisplay extends StatelessWidget {
       body: SafeArea(
         child: Center(
           child: Column(
-            children: const[
-
-
-              
-              Text('${topText}', 
-                style: TextStyle(
-                  color: Colors.black, 
-                  fontSize: 96
-                ),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                widget.topText,
+                style: const TextStyle(color: Colors.black, fontSize: 96),
               ),
 
-              switch(switching) {
-              case 0:   // No Switching - use equity
-                Text('${equity}', 
-                  style: TextStyle(
-                    color: Colors.black, 
-                    fontSize: 96
-                  ),
+              const SizedBox(height: 20),
+              if(!widget.switching)
+                Column(
+                  children: [
+                    Text(
+                      isAvailableDisplaying ? '${widget.available}' : '${widget.equity}',
+                      style: const TextStyle(color: Colors.black, fontSize: 96),
+                    ),
+                    ValencyEquitySwitcher(
+                      isEquity: !isAvailableDisplaying,
+                      onSwitch: switchType,
+                    ),
+                  ],
                 ),
-
-                break;
-              case 1:   // Switching
-                switch(isAvailableDisplaying) {
-                case 0:   // Display Equity Amount + Switcher
-                  Text('${equity}', 
-                    style: TextStyle(
-                      color: Colors.black, 
-                      fontSize: 96
-                    ),
-                  ),
-                  ValencyEquitySwitcher(
-                    true,
-                    switchType,
-                  ),
-
-                  break;
-                case 1:   // Display Available Amount + Switcher
-                  Text('${available}', 
-                    style: TextStyle(
-                      color: Colors.black, 
-                      fontSize: 96
-                    ),
-                  ),
-                  ValencyEquitySwitcher(
-                    false,
-                    switchType,
-                  ),
-
-                  break;
-                }
-
-                break;
-              }
-
-
-
             ],
           ),
         ),
@@ -94,40 +67,23 @@ class ValencyEquityDisplay extends StatelessWidget {
 }
 
 class ValencyEquitySwitcher extends StatelessWidget {
-  final bool equity;           // True if displaying equity, false if displaying available
-  final Function()? onSwitch;  // Detects button presses
-
   ValencyEquitySwitcher({
     super.key,
-    required this.equity,
+    required this.isEquity,
     required this.onSwitch,
   });
+
+  final bool isEquity;            // True if equity is being displayed
+  final VoidCallback? onSwitch;   // Detects button presses
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onSwitch,
-      child: 
-        switch(equity) {
-        case 0:
-          Text(
-            'Equity', 
-            style: TextStyle(
-              color: Colors.black, 
-              fontSize: 36
-            ),
-          ),
-          break;
-        case 1:
-          Text(
-            'Available', 
-            style: TextStyle(
-              color: Colors.black, 
-              fontSize: 36
-            ),
-          ),
-          break;
-        }
+      child: Text(
+        isEquity ? 'Equity' : 'Available',
+        style: const TextStyle(color: Colors.black, fontSize: 32),
+      ),
     );
   }
 }
