d4f82b26987a2de0b3570c95629bd4bd7c7de226
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-19 21:55:13 +1000

    Fixed bottombar formatting and switched to stateful widget

--- valencyapp/components/main/bottombar/bottombar.dart
+++ valencyapp/components/main/bottombar/bottombar.dart
@@ -1,58 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:valencyapp/components/bottombar/myportfoliobutton.dart';
-import 'package:valencyapp/components/bottombar/explorebutton.dart';
-import 'package:valencyapp/components/bottombar/mywalletbutton.dart';
-import 'package:valencyapp/components/bottombar/mypositionsbutton.dart';
-import 'package:valencyapp/components/bottombar/settingsbutton.dart';
+import 'package:valencyapp/components/main/bottombar/myportfoliobutton.dart';
+import 'package:valencyapp/components/main/bottombar/explorebutton.dart';
+import 'package:valencyapp/components/main/bottombar/mywalletbutton.dart';
+import 'package:valencyapp/components/main/bottombar/mypositionsbutton.dart';
+import 'package:valencyapp/components/main/bottombar/settingsbutton.dart';
 
-class ValencyBottomBar extends StatelessWidget {
-  final int focusedIcon;    // The button that is in focus: 0 = My Portfolio, 1 = Explore, 2 = My Wallet, 3 = My Positions, 4 = Settings
-
+class ValencyBottomBar extends StatefulWidget {
   const ValencyBottomBar({
-    super.key,
-    required this.focusedIcon,
+    super.key
   });
 
-  // I/O Controllers
-  void MyPortfolioPressed() {}  // Switch to my portfolio (if not already there)
-  void ExplorePressed() {}      // Switch to explore (if not already there)
-  void MyWalletPressed() {}     // Switch to my wallet (if not already there)
-  void MyPositionsPressed() {}  // Switch to my positions (if not already there)
-  void SettingsPressed() {}     // Switch to settings (if not already there)
+  @override
+  State<ValencyBottomBar> createState() => _ValencyBottomBarState();
+}
+
+class _ValencyBottomBarState extends State<ValencyBottomBar> {
+  int focusedIcon = 0;    // The button that is in focus: 0 = My Portfolio, 1 = Explore, 2 = My Wallet, 3 = My Positions, 4 = Settings
+
+  void _handleIconPress(int index) {
+    setState(() {
+      focusedIcon = index;
+    });
+
+    switch(index) {
+    case 0:   // Go to My Portfolio Screen
+      break;
+    case 1:   // Go to Explore Screen
+      break;
+    case 2:   // Go to My Wallet Screen
+      break;
+    case 3:   // Go to My Positions Screen
+      break;
+    case 4:   // Go to Settings Screen
+      break;
+    default:
+      break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Each button should be next to each other on the horizontal plane like in the designs
-    // Wrappers go here (wrappers should position each element in the correct position)
-    ValencyMyPortfolioButton(
-      onTap: MyPortfolioPressed,
-      active: focusedIcon == 0?
-        true:
-        false,
-    ),
-    ValencyExploreButton(
-      onTap: ExplorePressed,
-      active: focusedIcon == 1?
-        true:
-        false,
-    ),
-    ValencyMyWalletButton(
-      onTap: MyWalletPressed,
-      active: focusedIcon == 2?
-        true:
-        false,
-    ),
-    ValencyMyPositionsButton(
-      onTap: MyPositionsPressed,
-      active: focusedIcon == 3?
-        true:
-        false,
-    ),
-    ValencySettingsButton(
-      onTap: SettingsPressed,
-      active: focusedIcon == 4?
-        true:
-        false,
-    ),
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ValencyMyPortfolioButton(
+            onTap: () => _handleIconPress(0),
+            active: focusedIcon == 0,
+          ),
+          ValencyExploreButton(
+            onTap: () => _handleIconPress(1),
+            active: focusedIcon == 1,
+          ),
+          ValencyMyWalletButton(
+            onTap: () => _handleIconPress(2),
+            active: focusedIcon == 2,
+          ),
+          ValencyMyPositionsButton(
+            onTap: () => _handleIconPress(3),
+            active: focusedIcon == 3,
+          ),
+          ValencySettingsButton(
+            onTap: () => _handleIconPress(4),
+            active: focusedIcon == 4,
+          ),
+        ],
+      ),
+    );
   }
 }
