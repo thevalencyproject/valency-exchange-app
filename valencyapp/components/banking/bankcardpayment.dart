ef408c9061395951491766f6d31e71d9366d3f00
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-21 08:09:25 +1000

    Fixed ValencyDislpayBankCardDetails Widget and implemented new card button

--- valencyapp/components/banking/bankcardpayment.dart
+++ valencyapp/components/banking/bankcardpayment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:valencyapp/components/textfield.dart';
+import 'package:valencyapp/components/textbutton.dart';
 
 class ValencyBankCardPayment extends StatelessWidget {
   ValencyBankCardPayment({
@@ -90,48 +91,82 @@ class ValencyBankCardPayment extends StatelessWidget {
 }
 
 class ValencyDisplayBankCardDetails extends StatelessWidget {
-  final String sessionId;        // The session ID
-  final String passcode;         // The passcode to access the session
-
-  // The system does NOT send CCV numbers in display bank card detail requests for security reasons!!!
-  String getCardNumber() {}
-  String getExpiry() {}
-  String getNameOnCard() {}
-
   const ValencyDisplayBankCardDetails({
     super.key,
-    required this.sessionId,
-    required this.passcode,
+    required this.cardNumber,
+    required this.expiryDate,
+    required this.nameOnCard,
+    required this.numberController,
+    required this.expiryController,
+    required this.ccvController,
+    required this.nameOnCardController,
   });
+
+  final String cardNumber;                             // Passthrough - Card Number (should show first and last 4 digits for security)
+  final String expiryDate;                             // Passthrough - Card Expiry Date
+  final String nameOnCard;                             // Passthrough - Name on the Bank Card
+  final TextEditingController numberController;        // If user wants to use a different card - The controller that can access the card number
+  final TextEditingController expiryController;        // If user wants to use a different card - The controller that can access the cards expiry date
+  final TextEditingController ccvController;           // If user wants to use a different card - The controller that can access the cards ccv
+  final TextEditingController nameOnCardController;    // If user wants to use a different card - The controller that can access the name on the card
 
   @override
   Widget build(BuildContext context) {
     return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Saved Card Details:',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          
+          SizedBox(height: 10),
 
-      Text(
-        'Card Number: + ${getCardNumber()}', 
-        style: TextStyle(
-          color: Colors.black, 
-          fontSize: 32
-        ),
+          Text(
+            'Card Number: $cardNumber',
+            style: TextStyle(fontSize: 32),
+          ),
+
+          SizedBox(height: 2),
+
+          Text(
+            'Expiry Date: $expiryDate',
+            style: TextStyle(fontSize: 32),
+          ),
+
+          SizedBox(height: 2),
+
+          Text(
+            'Name On Card: $nameOnCard',
+            style: TextStyle(fontSize: 32),
+          ),
+
+          SizedBox(height: 20),
+
+          ValencyTextButton(  
+            onTap: () {       // When pressed, replace current widget with ValencyBankCardPayment widget and pass all the text field controllers
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ValencyBankCardPayment(
+                  numberController: numberController, 
+                  expiryController: expiryController, 
+                  ccvController: ccvController, 
+                  nameOnCardController: nameOnCardController
+                  )
+                ),
+              );
+            },
+            buttonColor: Colors.blue,
+            buttonText: 'Use another card',
+          ),
+        ],
       ),
-
-      Text(
-        'Expiry: + ${getBSB()}', 
-        style: TextStyle(
-          color: Colors.black, 
-          fontSize: 32
-        ),
-      ),
-
-      Text(
-        'Name on Card: + ${getNameOnCard()}', 
-        style: TextStyle(
-          color: Colors.black, 
-          fontSize: 32
-        ),
-      ),
-
     );
   }
 }
