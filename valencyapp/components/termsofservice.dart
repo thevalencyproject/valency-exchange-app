cec3bf5bd5aa4c08fd76ff1c554f80ac82ba4131
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-12-23 15:34:15 +1000

    Implemented Stateful http plaintext getter widget

--- valencyapp/components/termsofservice.dart
+++ valencyapp/components/termsofservice.dart
@@ -1,20 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class ValencyTOS extends StatelessWidget {
+class ValencyTOS extends StatefulWidget {
+  const ValencyTOS({
+    Key? key,
+    required this.url, 
+    required this.onScrolledToBottom,
+  }) : super(key: key);
 
-  const ValencyTOS({ super.key, });
+  final String url;                     // The url with the plaintext TOS
+  final Function onScrolledToBottom;    // The function that is ran when scrolled to bottom
+
+  @override
+  _ValencyTOSState createState() => _ValencyTOSState();
+}
+
+class _ValencyTOSState extends State<ValencyTOS> {
+  String _textData = "";                                            // Place to store fetched text
+  bool _isAtBottom = false;                                         // Shouldn't spawn widget already scrolled to bottom
+  final ScrollController _scrollController = ScrollController();    // Handles scroll detection
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchTextData();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  Future<void> _fetchTextData() async {
+    try {
+      final response = await http.get(Uri.parse(widget.url));
+      if(response.statusCode = 200) {
+        setState(() {
+          _textData = response.body;
+        });
+      } else {
+        // Handle error
+        
+      }
+    } catch (e) {
+      // Handle error
+    }
+  }
+
+  void _scrollListener() {
+    if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent && !_isAtBottom) {
+      widget.onScrolledToBottom();
+      setState(() {
+        _isAtBottom = true;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    Container(
-      padding: const EdgeInsets.all(25),
-      margin: const EdgeInsets.symmetric(horizontal: 25),
+    return Container(
+      padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.blue),
+        borderRadius: BorderRadius.circular(5),
       ),
+      child: _textData.isEmpty
+        ? CircularProgressIndicator()
+        : SingleChildScrollView(
+          controller: _scrollController,
+          child: Text(_textData),
+        ),
+    
     );
   }
 }
 
-// STILL NEEDS TO GET AND DISPLAY ACTUAL TOS + MUST BE CHANGED TO CORRECT SIZE (TAKE UP MOST OF SCREEN LIKE DESIGN)
