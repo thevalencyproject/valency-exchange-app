1db69f8d3d272ce55c41de4e24cc55ef68100eb5
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2024-01-03 18:21:48 +1000

    Converted to DisplayRange

--- valencyapp/components/main/valuegraph/basegraph.dart
+++ valencyapp/components/main/valuegraph/basegraph.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
-
-enum SelectedRange { day, week, month, threeMonths, year, max }
+import 'package:valencyapp/structures/range.dart';
 
 // Opening rate is optional - range should be a selection from SelectedRange, and data is a list of doubles over said time period
 class ValencyBaseGraph extends StatelessWidget {
@@ -13,7 +12,7 @@ class ValencyBaseGraph extends StatelessWidget {
   }) : super(key: key);
 
   final List<double> data;
-  final SelectedRange selectedRange;
+  final DisplayRange selectedRange;
   final double? openingRate;
 
   @override
