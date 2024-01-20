f188d4230813dfc7ee530a7ebed30f807f4b4058
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2024-01-20 16:33:08 +1000

    Added Future Supabase Initialisation

--- valencyapp/main.dart
+++ valencyapp/main.dart
@@ -3,13 +3,19 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:valencyapp/base_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();                                                               // Ensures app is initialized
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);                                  // Disables Bottom Bar and Status Bar
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);   // Ensures app runs in portrait
 
-  runApp(MyApp());  // Run the app
+  await Supabase.initialize(  // Initialise with supabase
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_ANON_KEY',
+  );
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -49,58 +55,3 @@ class _MyHomePageState extends State<MyHomePage> {
     
   }
 }
-
-
-/*
-
-class MyAppState extends ChangeNotifier {
-  // Logic goes here
-}
-
-class PortfolioPage {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('My Portfolio'),                                      // My Portfolio
-          child: Text('$' '${appState.userinfo.portfolioValue}'),           // $2000.00
-          if(appState.userinfo.isPortfolioValueUp) {
-            child: Icon();          // Up Arrow Icon
-            child: Text('+$' '${appState.userinfo.}' 'appState.filterinfo.scope'),
-          } else {
-            child: Icon();          // Down Arrow Icon
-            child: Text('${appState.userinfo.isPortfolioValueUp}' '$' ''),
-          }
-          
-        )
-      ]
-    )
-  }
-}  // Contains the user portfolio - includes any open positions (short, put, call, etc)
-class WalletPage {
-  @override
-}     // Contains the user wallet and balances + withdraw options
-class ExplorePage {
-  @override
-}    // Contains all the cryptocurrencies - you can buy cryptocurrencies here / swap them for already owned assets
-class TradingPage {
-  @override
-}    // Contains the users current (open) and previous (closed) positions + lets the user open new positions
-class SettingsPage {
-  @override
-}   // Contains settings
-class AddFundsPage {
-  @override
-}   // Contains Add Funds Page
-class WithdrawFundsPage {
-  @override
-}   // Contains Withdraw Funds Page
-class WithdrawAssetForFundsPage {
-  @override
-}   // Contains Withdraw Asset For Funds Page - Sell asset immediatly for funds - don't sell asset, get funds in account, then withdraw
-
- */
